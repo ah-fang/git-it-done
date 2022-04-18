27102de1dfd00f98c32bc865e0d21de63cbf475b
ah-fang <94493319+ah-fang@users.noreply.github.com>
2022-04-18 16:21:49 -0700

    redirects to homepage

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    //get repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        //display the repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        //...or redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(response) {
@@ -17,10 +32,9 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
-    console.log(repo);
 };
 
 var displayIssues = function(issues) {
@@ -73,4 +87,4 @@ var displayWarning = function(repo) {
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
